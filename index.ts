80fe410c1f0cf3c451aa71d4a995a7a06ad923c5
AsimMahmood151 <masim55@gmail.com>
2024-04-18 00:02:18 +0500

    first comment
    Push First Project

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const   object=await inquirer.prompt([{message:"Enter First Number",type:"number",name:"FirstNumber"},
+{message:"Enter Second Number",type:"number",name:"SecondNumber"},
+{message:"Select Operation",type:"list",name:"operator",choices:["Add","Subtract","Multiply","Division"]}
+]);
+//console.log(object);
+if (object.operator==="Add"){
+    console.log(object.FirstNumber+object.SecondNumber);
+}
+else if (object.operator==="Subtract"){
+    console.log(object.FirstNumber-object.SecondNumber);
+}
+else if (object.operator==="Multiply"){
+    console.log(object.FirstNumber*object.SecondNumber);
+}
+else if (object.operator==="Division"){
+    console.log(object.FirstNumber/object.SecondNumber);
+}
+else
+console.log("Select Operator from List");
